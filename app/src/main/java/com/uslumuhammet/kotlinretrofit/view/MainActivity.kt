20f07d7ebb7a1933efd4d5cf7751cc133e9059ae
muhammetuslu78 <muhammetuslu78@gmail.com>
2022-09-14 11:59:18 +0300

    Make fourth commit

--- app/src/main/java/com/uslumuhammet/kotlinretrofit/view/MainActivity.kt
+++ app/src/main/java/com/uslumuhammet/kotlinretrofit/view/MainActivity.kt
@@ -99,7 +99,7 @@ class MainActivity : AppCompatActivity(), CryptoAdapter.Listener {
     override fun OnItemClick(crypto: Crypto) {
         Toast.makeText(this,"Clicked ${crypto.currency}",Toast.LENGTH_LONG).show()
         println("Deneme")
-        test  test etme test et
+        kartal gol gol gol kartal gol gol gol
     }
 
     private fun handleResponse(cryptoList2: List<Crypto>)
